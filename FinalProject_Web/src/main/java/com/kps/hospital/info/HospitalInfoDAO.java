008ab4f9703fcc3f3d07bf60d72d14f8f97566ec
yjson <yjson@192.168.0.7>
2021-03-25 15:29:09 +0900

    DAO

--- FinalProject_Web/src/main/java/com/kps/hospital/info/HospitalInfoDAO.java
+++ FinalProject_Web/src/main/java/com/kps/hospital/info/HospitalInfoDAO.java
@@ -1,5 +1,14 @@
 package com.kps.hospital.info;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HospitalInfoDAO {
 
+	@Autowired
+	private SqlSession ss;
+	
+	// 
 }
