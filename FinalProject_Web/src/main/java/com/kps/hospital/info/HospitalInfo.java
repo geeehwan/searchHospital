20d889e27fec9f4242db1973424162e7e3e985cf
sundooedu <sundooedu@DESKTOP-Q1L1177>
2021-03-25 15:26:23 +0900

    객체생성

--- FinalProject_Web/src/main/java/com/kps/hospital/info/HospitalInfo.java
+++ FinalProject_Web/src/main/java/com/kps/hospital/info/HospitalInfo.java
@@ -1,44 +1,251 @@
 package com.kps.hospital.info;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class HospitalInfo {
-
-	@Autowired
-	private SqlSession ss;
-
-	// 파일을 가지고와서 디비에 넣고싶다
-	public String fileToDB() {
-
-		BufferedReader br = null;
-		try {
-
-			FileInputStream fis = new FileInputStream("/Users/yjson/Downloads/s_hospital.txt");
-			InputStreamReader isr = new InputStreamReader(fis, "utf-8");
-			br = new BufferedReader(isr);
-
-			String line = null;
-			StringBuffer sb = new StringBuffer(); // 이거 여기 필요한가?
-
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-
-			return sb.toString();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			return null;
-		}
-		// br.close(); 언제해
+	private BigDecimal dutyno;
+	private String dutyaddr;
+	private BigDecimal dutyeryn; 
+	private String dutyname;
+	private String dutytel1;
+	private BigDecimal wgs84lat;
+	private BigDecimal wgs84lon;
+	private String dutytime1s;
+	private String dutytime1c;
+	private String dutytime2s;
+	private String dutytime2c;
+	private String dutytime3s;
+	private String dutytime3c;
+	private String dutytime4s;
+	private String dutytime4c;
+	private String dutytime5s;
+	private String dutytime5c;
+	private String dutytime6s;
+	private String dutytime6c;
+	private String dutytime7s;
+	private String dutytime7c;
+	private String dutytime8s;
+	private String dutytime8c;
+	
+	public HospitalInfo() {
+		// TODO Auto-generated constructor stub
 	}
+	
+	public HospitalInfo(BigDecimal dutyno, String dutyaddr, BigDecimal dutyeryn, String dutyname, String dutytel1,
+			BigDecimal wgs84lat, BigDecimal wgs84lon, String dutytime1s, String dutytime1c, String dutytime2s,
+			String dutytime2c, String dutytime3s, String dutytime3c, String dutytime4s, String dutytime4c,
+			String dutytime5s, String dutytime5c, String dutytime6s, String dutytime6c, String dutytime7s,
+			String dutytime7c, String dutytime8s, String dutytime8c) {
+		super();
+		this.dutyno = dutyno;
+		this.dutyaddr = dutyaddr;
+		this.dutyeryn = dutyeryn;
+		this.dutyname = dutyname;
+		this.dutytel1 = dutytel1;
+		this.wgs84lat = wgs84lat;
+		this.wgs84lon = wgs84lon;
+		this.dutytime1s = dutytime1s;
+		this.dutytime1c = dutytime1c;
+		this.dutytime2s = dutytime2s;
+		this.dutytime2c = dutytime2c;
+		this.dutytime3s = dutytime3s;
+		this.dutytime3c = dutytime3c;
+		this.dutytime4s = dutytime4s;
+		this.dutytime4c = dutytime4c;
+		this.dutytime5s = dutytime5s;
+		this.dutytime5c = dutytime5c;
+		this.dutytime6s = dutytime6s;
+		this.dutytime6c = dutytime6c;
+		this.dutytime7s = dutytime7s;
+		this.dutytime7c = dutytime7c;
+		this.dutytime8s = dutytime8s;
+		this.dutytime8c = dutytime8c;
+	}
+
+	public BigDecimal getDutyno() {
+		return dutyno;
+	}
+
+	public void setDutyno(BigDecimal dutyno) {
+		this.dutyno = dutyno;
+	}
+
+	public String getDutyaddr() {
+		return dutyaddr;
+	}
+
+	public void setDutyaddr(String dutyaddr) {
+		this.dutyaddr = dutyaddr;
+	}
+
+	public BigDecimal getDutyeryn() {
+		return dutyeryn;
+	}
+
+	public void setDutyeryn(BigDecimal dutyeryn) {
+		this.dutyeryn = dutyeryn;
+	}
+
+	public String getDutyname() {
+		return dutyname;
+	}
+
+	public void setDutyname(String dutyname) {
+		this.dutyname = dutyname;
+	}
+
+	public String getDutytel1() {
+		return dutytel1;
+	}
+
+	public void setDutytel1(String dutytel1) {
+		this.dutytel1 = dutytel1;
+	}
+
+	public BigDecimal getWgs84lat() {
+		return wgs84lat;
+	}
+
+	public void setWgs84lat(BigDecimal wgs84lat) {
+		this.wgs84lat = wgs84lat;
+	}
+
+	public BigDecimal getWgs84lon() {
+		return wgs84lon;
+	}
+
+	public void setWgs84lon(BigDecimal wgs84lon) {
+		this.wgs84lon = wgs84lon;
+	}
+
+	public String getDutytime1s() {
+		return dutytime1s;
+	}
+
+	public void setDutytime1s(String dutytime1s) {
+		this.dutytime1s = dutytime1s;
+	}
+
+	public String getDutytime1c() {
+		return dutytime1c;
+	}
+
+	public void setDutytime1c(String dutytime1c) {
+		this.dutytime1c = dutytime1c;
+	}
+
+	public String getDutytime2s() {
+		return dutytime2s;
+	}
+
+	public void setDutytime2s(String dutytime2s) {
+		this.dutytime2s = dutytime2s;
+	}
+
+	public String getDutytime2c() {
+		return dutytime2c;
+	}
+
+	public void setDutytime2c(String dutytime2c) {
+		this.dutytime2c = dutytime2c;
+	}
+
+	public String getDutytime3s() {
+		return dutytime3s;
+	}
+
+	public void setDutytime3s(String dutytime3s) {
+		this.dutytime3s = dutytime3s;
+	}
+
+	public String getDutytime3c() {
+		return dutytime3c;
+	}
+
+	public void setDutytime3c(String dutytime3c) {
+		this.dutytime3c = dutytime3c;
+	}
+
+	public String getDutytime4s() {
+		return dutytime4s;
+	}
+
+	public void setDutytime4s(String dutytime4s) {
+		this.dutytime4s = dutytime4s;
+	}
+
+	public String getDutytime4c() {
+		return dutytime4c;
+	}
+
+	public void setDutytime4c(String dutytime4c) {
+		this.dutytime4c = dutytime4c;
+	}
+
+	public String getDutytime5s() {
+		return dutytime5s;
+	}
+
+	public void setDutytime5s(String dutytime5s) {
+		this.dutytime5s = dutytime5s;
+	}
+
+	public String getDutytime5c() {
+		return dutytime5c;
+	}
+
+	public void setDutytime5c(String dutytime5c) {
+		this.dutytime5c = dutytime5c;
+	}
+
+	public String getDutytime6s() {
+		return dutytime6s;
+	}
+
+	public void setDutytime6s(String dutytime6s) {
+		this.dutytime6s = dutytime6s;
+	}
+
+	public String getDutytime6c() {
+		return dutytime6c;
+	}
+
+	public void setDutytime6c(String dutytime6c) {
+		this.dutytime6c = dutytime6c;
+	}
+
+	public String getDutytime7s() {
+		return dutytime7s;
+	}
+
+	public void setDutytime7s(String dutytime7s) {
+		this.dutytime7s = dutytime7s;
+	}
+
+	public String getDutytime7c() {
+		return dutytime7c;
+	}
+
+	public void setDutytime7c(String dutytime7c) {
+		this.dutytime7c = dutytime7c;
+	}
+
+	public String getDutytime8s() {
+		return dutytime8s;
+	}
+
+	public void setDutytime8s(String dutytime8s) {
+		this.dutytime8s = dutytime8s;
+	}
+
+	public String getDutytime8c() {
+		return dutytime8c;
+	}
+
+	public void setDutytime8c(String dutytime8c) {
+		this.dutytime8c = dutytime8c;
+	}
+	
+	
 }
+
